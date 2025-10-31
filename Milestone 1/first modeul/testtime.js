8fc29c3ba2d32fc975379f33df754f6bfabb1dc5
rifat-54 <rifat999por@gmail.com>
2025-10-31 18:08:58 +0600

    added test file

--- Milestone 1/first modeul/testtime.js
+++ Milestone 1/first modeul/testtime.js
@@ -1,1 +1,13 @@
-console.log('test time');
+// const startTime=performance.now()
+
+console.time("task")
+
+for(let i=0;i<=50;i++){
+    console.log(i);
+}
+
+// const endTime=performance.now()
+
+// console.log(`this code took ${endTime-startTime} - ms`);
+
+console.timeEnd("task")
